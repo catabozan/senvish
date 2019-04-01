e5b14e8aeaa941d1010e032557197070e13a7ec1
catabozan <catabozan@protonmail.com>
2019-04-02 00:22:40 +0300

    added comments to js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var buttons = ["burger"];
 
+//adds event listeners on all buttons/menus
 for(var i = 0; i < buttons.length; i++){
 	addEventListenerToButton(buttons[i]);
 }
 
 
 
+//this adds event listeners
 function addEventListenerToButton(item){
+	//select itemMenu
 	var mainButton = document.querySelector("#" + item + "Button");
+	//select itemMenu close-button
 	var closeButton = document.querySelector("#" + item + "Menu" + " .close-menu-button");
+
+	//add event listeners
 	mainButton.addEventListener("click", function(){
 		toggleMenu(item);
 	});
@@ -17,9 +23,14 @@ function addEventListenerToButton(item){
 	});
 };
 
+//this toggles menu
 function toggleMenu(item){
+	//select itemMenu
 	var container = document.querySelector("#" + item +"Menu");
+	//select menu-wrapper inside itemMenu
 	var wrapper = document.querySelector("#" + item +"Menu" + " .menu-wrapper");
+
+	//toggle classes
 	wrapper.classList.toggle("visible");
 	container.classList.toggle("dark");
 };
