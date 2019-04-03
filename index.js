d2611c47874bdfa7b57fb8acb7a548e0618df823
catabozan <catabozan@protonmail.com>
2019-04-03 23:28:30 +0300

    added burger menu and logic

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 var buttons = ["burger"];
 
+//selects menu items
+var bread = document.querySelectorAll(".bread .item-name");
+var meat = document.querySelectorAll(".meat .item-name");
+var veggie = document.querySelectorAll(".veggie .item-name");
+var SOS = document.querySelectorAll(".SOS .item-name");
+
+var oneItemMenu = [bread, meat];
+var moreItemMenu = [veggie, SOS];
+
+//select homePage
+var homePage = document.querySelector("#homePage");
+//select body
+var body = document.querySelector("body");
+
 //adds event listeners on all buttons/menus
 for(var i = 0; i < buttons.length; i++){
 	addEventListenerToButton(buttons[i]);
 }
 
+//adds event listeners on all single item menus
+for(var i = 0; i < oneItemMenu.length; i++){
+	for(var j = 0; j < oneItemMenu[i].length; j++){
+		addEventListenerToOneItemMenuItem(oneItemMenu[i][j], oneItemMenu[i])
+	}
+}
 
+//adds event listeners on all multiple item menus
+for(var i = 0; i < moreItemMenu.length; i++){
+	for(var j = 0; j < moreItemMenu[i].length; j++){
+		addEventListenerToMoreItemMenuItem(moreItemMenu[i][j])
+	}
+}
+
+function addEventListenerToMoreItemMenuItem(item){
+	item.addEventListener("click", function(){
+		this.classList.toggle("selected");
+	})
+}
+
+
+//add event listeners on one item per menu items
+function addEventListenerToOneItemMenuItem(item, menu){
+	item.addEventListener("click", function(){
+		//removes "selected" class on all items
+		for(var i = 0; i < menu.length; i++){
+			menu[i].classList.remove("selected");
+		}
+		//adds "selected" class on clicked item
+		this.classList.add("selected");
+	})
+}
 
 //this adds event listeners
 function addEventListenerToButton(item){
@@ -33,4 +78,5 @@ function toggleMenu(item){
 	//toggle classes
 	wrapper.classList.toggle("visible");
 	container.classList.toggle("dark");
+	homePage.classList.toggle("blurred");
 };
